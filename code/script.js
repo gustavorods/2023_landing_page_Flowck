35ebdd29294a834985e7fb8f0b0cc5aff6a0606d
Gustavo <gustavo.rlsilva07@gmail.com>
2023-05-04 21:12:51 -0300

    first commit

---
+++ code/script.js
@@ -0,0 +1,8 @@
+function enviar(nome, email) {
+    var nome, email;
+
+    nome = nome.value;
+    email = email.value;
+
+    alert(`${nome}, as seguintes informações foram enviadas: Nome: ${nome} / Email: ${email}`)
+}
